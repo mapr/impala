e4bf9474c9d4ca7181916fd10e6d38baa9043923
Parth Chandra <pchandra@maprtech.com>
2014-04-10 18:12:52 -0700

    fix issues with M7 tables, avro, and hive-site.xml
    Signed-off-by: IVarha <ivarga@maprtech.com>

--- mapr/conf/set_path_env.sh
+++ mapr/conf/set_path_env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 setCLASSPATH() {
     local h=$HADOOP_HOME/lib
 
@@ -9,6 +10,9 @@ setCLASSPATH() {
     CLASSPATH+=:$(getPath $HIVE_HOME/lib/*.jar)
     CLASSPATH+=:$(getPath $HADOOP_HOME/lib/*.jar)
     CLASSPATH+=:$(getPath $HBASE_HOME/*.jar)
+
+    # add MAPR jars to classpath (for bug 13771)
+    CLASSPATH+=:$(getPath ${MAPR_HOME}/lib/*.jar)
 
     # set the class path to pick up configuration files
     CLASSPATH+=:/opt/mapr/impala/impala-1.2.3/conf:$HIVE_SITE_DIR:$HADOOP_HOME/conf:$HBASE_HOME/conf:
@@ -31,6 +35,45 @@ getPath() {  # <list of things on path>
         path+=:$p
     done
     echo $path
+}
+
+#
+# For hive-site.xml 
+#
+
+createSiteXML() { # <uri>
+cat <<- EOF > ${IMPALA_HOME}/conf/hive-site.xml
+<!--  GENERATED automatically by impalaserver -->
+<configuration>
+   <property>
+     <name>hive.metastore.uris</name>
+     <value>$HIVE_METASTORE_URI</value>
+   </property>
+</configuration>
+EOF
+}
+
+
+findHiveSite()
+{
+    # CASE: we have our own XML which was not generated automatically, use it
+    if [ -f ${IMPALA_HOME}/conf/hive-site.xml ]   \
+       && ! grep -q GENERATED ${IMPALA_HOME}/conf/hive-site.xml; then
+         echo "${IMPALA_HOME}/conf"
+ 
+    # CASE: we have an env variable, create our own site.xml
+    elif [ ! -z "$HIVE_METASTORE_URI" ]; then
+         createSiteXML
+         echo "${IMPALA_HOME}/conf"
+       
+    # CASE: valid HIVE xml, use it
+    elif grep -q hive.metastore.uris $HIVE_HOME/conf/hive-site.xml; then
+        echo $HIVE_HOME/conf
+
+    # OTHERWISE, error
+    else
+        echo ""
+    fi
 }
 
 
