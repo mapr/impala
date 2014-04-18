f76b22758b14cb7a14a085087a52b45873413fb6
Parth Chandra <pchandra@maprtech.com>
2014-04-17 18:44:03 -0700

    fixes for warden configuration and additional params for catalog server startup

--- mapr/conf/env.sh
+++ mapr/conf/env.sh
@@ -28,7 +28,11 @@ IMPALA_SERVER_ARGS=" \
 
 # These impact the state store daemon and can be optionally changed
 IMPALA_STATE_STORE_ARGS=" -log_dir=${IMPALA_LOG_DIR} -state_store_port=${IMPALA_STATE_STORE_PORT} -catalog_service_host=${CATALOG_SERVICE_HOST} "
-IMPALA_CATALOG_ARGS=" -log_dir=${IMPALA_LOG_DIR} "
+IMPALA_CATALOG_ARGS=" -log_dir=${IMPALA_LOG_DIR} \
+    -state_store_port=${IMPALA_STATE_STORE_PORT} \
+    -use_statestore \
+    -state_store_host=${IMPALA_STATE_STORE_HOST} \
+"
 
 # for troubleshooting
 ENABLE_CORE_DUMPS=false
