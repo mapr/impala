7a9f5f5fdb41209673f76f9378940802790b1922
Parth Chandra <pchandra@maprtech.com>
2014-04-21 18:13:24 -0700

    catch exception at for bogus request o update catalog cache when creating UDFs with the IF NOT EXISTS clause

--- fe/src/main/java/com/cloudera/impala/catalog/ImpaladCatalog.java
+++ fe/src/main/java/com/cloudera/impala/catalog/ImpaladCatalog.java
@@ -245,7 +245,11 @@ public class ImpaladCatalog extends Catalog {
     }
     lastSyncedCatalogVersion_ = newCatalogVersion;
     // Cleanup old entries in the log.
-    catalogDeltaLog_.garbageCollect(lastSyncedCatalogVersion_);
+      try{
+          catalogDeltaLog_.garbageCollect(lastSyncedCatalogVersion_);
+      }catch(IllegalArgumentException e){
+           // Do nothing
+      }
     isReady_.set(true);
 
     // Notify all the threads waiting on a catalog update.
@@ -480,11 +484,17 @@ public class ImpaladCatalog extends Catalog {
   private void addCatalogObject(TCatalogObject catalogObject)
       throws TableLoadingException, DatabaseNotFoundException {
     // This item is out of date and should not be applied to the catalog.
-    if (catalogDeltaLog_.wasObjectRemovedAfter(catalogObject)) {
-      LOG.debug(String.format("Skipping update because a matching object was removed " +
-          "in a later catalog version: %s", catalogObject));
-      return;
-    }
+    try {
+      if (catalogDeltaLog_.wasObjectRemovedAfter(catalogObject)) {
+        LOG.debug(String.format("Skipping update because a matching object was removed " +
+            "in a later catalog version: %s", catalogObject));
+        return;
+      }
+   }catch(IllegalArgumentException e){
+        // Do nothing
+        return;
+   }
+
 
     switch(catalogObject.getType()) {
       case DATABASE:
